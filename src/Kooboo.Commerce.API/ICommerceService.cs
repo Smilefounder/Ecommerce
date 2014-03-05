af21ed803687b8c1a1880411d7e96d5fd676f561
chunsheng <chunsheng@kooboo.com>
2014-03-05 09:17:20 +0800

    add sample pages for commerce

---
+++ src/Kooboo.Commerce.API/ICommerceService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kooboo;
+using Kooboo.Commerce.Accounts;
+using Kooboo.Commerce.Brands;
+using Kooboo.Commerce.Categories;
+using Kooboo.Commerce.Customers;
+using Kooboo.Commerce.EAV;
+using Kooboo.Commerce.Events;
+using Kooboo.Commerce.Locations;
+using Kooboo.Commerce.Orders;
+using Kooboo.Commerce.Payments;
+using Kooboo.Commerce.Pricing;
+using Kooboo.Commerce.Products;
+using Kooboo.Commerce.Promotions;
+using Kooboo.Commerce.ShoppingCarts;
+
+namespace Kooboo.Commerce.API
+{
+    public interface ICommerceService
+    {
+        IEnumerable<string> GetAllCommerceInstances();
+
+        IEnumerable<Category> GetAllCategories(string instance, int level = 1);
+
+        IEnumerable<Category> GetSubCategories(string instance, int parentCategoryId);
+
+        IEnumerable<Brand> GetAllBrands(string instance);
+
+        IPagedList<Product> SearchProducts(string instance, string userInputs, int pageIndex = 0, int pageSize = 50);
+
+        Product GetProductById(string instace, int id);
+    }
+}
---
+++ src/Kooboo.Commerce.API/LocalCommerceService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kooboo.CMS.Common.Runtime.Dependency;
+
+namespace Kooboo.Commerce.API
+{
+    [Dependency(typeof(ICommerceService))]
+    public class LocalCommerceService : ICommerceService
+    {
+        public IEnumerable<string> GetAllCommerceInstances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Categories.Category> GetAllCategories(string instance, int level = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Categories.Category> GetSubCategories(string instance, int parentCategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Brands.Brand> GetAllBrands(string instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPagedList<Products.Product> SearchProducts(string instance, string userInputs, int pageIndex = 0, int pageSize = 50)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Products.Product GetProductById(string instace, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
