504ea689efd6cfb9027a829ec60758f0a3feb2a2
chunsheng <chunsheng@kooboo.com>
2014-04-21 13:36:33 +0800

    add hal context and env privder

---
+++ src/Kooboo.Commerce.API.HAL/HalContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Kooboo.Commerce.API.HAL
+{
+    public class HalContext
+    {
+        public HttpContext WebContext { get; set; }
+        public object Data { get; set; }
+
+        public ResourceDescriptor RequestResource { get; set; }
+    }
+}
---
+++ src/Kooboo.Commerce.API.HAL/IContextEnviromentProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kooboo.Commerce.API.HAL
+{
+    public interface IContextEnviromentProvider
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+
+        bool IsContextRunInEnviroment(HalContext context);
+    }
+}
