62a6a2cce62639a7dbe480624fb709acc4ac1bcd
chunsheng <chunsheng@kooboo.com>
2014-05-08 13:45:52 +0800

    add cms site url map (hard code)

--- src/Kooboo.Commerce.API.CmsSite/CmsSiteUrlResolver.cs
+++ src/Kooboo.Commerce.API.CmsSite/CmsSiteUrlResolver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 using Kooboo.Commerce.API.HAL;
 
 namespace Kooboo.Commerce.API.CmsSite
@@ -15,44 +16,58 @@ namespace Kooboo.Commerce.API.CmsSite
             var link = resource.Links.FirstOrDefault(o => o.Rel.ToLower() == linkRelName.ToLower());
             if (link != null)
             {
-                var type = resource.GetType();
-                if (type == typeof(Categories.Category))
-                    return MapCategoryUrl(resource, link);
-                else if (type == typeof(Brands.Brand))
-                    return MapBrandUrl(resource, link);
-                else if (type == typeof(Products.Product))
-                    return MapProductUrl(resource, link);
-                else if (type == typeof(ShoppingCarts.ShoppingCart))
-                    return MapShoppingCartUrl(resource, link);
-                else if (type == typeof(Orders.Order))
-                    return MapOrderUrl(resource, link);
+                Uri linkUrl = MockUri(link.Href);
+                // we ignore the 1,2 segements, which stand for "/" and "{commerce instance}"
+                if (linkUrl.Segments.Length > 2)
+                {
+                    var qs = HttpUtility.ParseQueryString(linkUrl.Query);
+                    List<string> path = new List<string>();
+                    switch(linkUrl.Segments[2].TrimEnd('/').ToLower())
+                    {
+                        case "product":
+                            path.Add("product");
+                            if(linkUrl.Segments.Length > 3)
+                            switch(linkUrl.Segments[3].TrimEnd('/').ToLower())
+                            {
+                                case "list":
+                                    if(qs.AllKeys.Contains("categoryId"))
+                                    {
+                                        path.Add("category");
+                                        path.Add(qs["categoryId"]);
+                                        qs.Remove("categoryId");
+                                    }
+                                    break;
+                            }
+                            break;
+
+                    }
+                    if(qs.AllKeys.Contains("pageIndex"))
+                    {
+                        int pidx = 0;
+                        int.TryParse(qs["pageIndex"], out pidx);
+                        if(pidx <= 0)
+                        {
+                            qs.Remove("pageIndex");
+                            if (qs.AllKeys.Contains("pageSize"))
+                                qs.Remove("pageSize");
+                        }
+                    }
+                    var nurl = string.Join("/", path.ToArray());
+                    if(qs.Keys.Count > 0)
+                    {
+                        nurl += "?" + string.Join("&", qs.AllKeys.Select(o => string.Format("{0}={1}", o, qs[o])).ToArray());
+                    }
+                    return nurl;
+                }
             }
             return url;
         }
 
-        private static string MapCategoryUrl(IItemResource resource, Link link)
+        private static Uri MockUri(string href)
         {
-            return link.Href;
-        }
-
-        private static string MapBrandUrl(IItemResource resource, Link link)
-        {
-            return link.Href;
-        }
-
-        private static string MapProductUrl(IItemResource resource, Link link)
-        {
-            return link.Href;
-        }
-
-        private static string MapShoppingCartUrl(IItemResource resource, Link link)
-        {
-            return link.Href;
-        }
-
-        private static string MapOrderUrl(IItemResource resource, Link link)
-        {
-            return link.Href;
+            if (!href.StartsWith("/"))
+                href = "/" + href;
+            return new Uri(string.Format("http://localhost{0}", href), UriKind.Absolute);
         }
     }
 }
