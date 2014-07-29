a5f8061015af935fa5cffb71db7738fd77cb1bc3
Mouhong <mouhong@kooboo.com>
2014-07-29 20:05:11 +0800

    Remove some unused classes

--- src/Kooboo.Commerce/Products/CustomFieldExtensions.cs
+++ src/Kooboo.Commerce/Products/CustomFieldExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Kooboo.Commerce.Products
 {
-    public static class CustomFieldValuesExtensions
+    public static class CustomFieldExtensions
     {
         public static string GetValue(this IEnumerable<ProductCustomField> fieldValues, string fieldName)
         {
